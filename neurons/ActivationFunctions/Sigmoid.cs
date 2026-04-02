2f71acc304abae11c50a1fcccd842dcec788346a
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-02 04:03:10 +0200

    feat: implementa metodo che calcola la derivata della funzione Sigmoid

--- neurons/ActivationFunctions/Sigmoid.cs
+++ neurons/ActivationFunctions/Sigmoid.cs
@@ -10,5 +10,10 @@ namespace neurons.ActivationFunctions
         {
             return 1.0 / (1.0 + Math.Exp(-sum));
         }
+
+        public static double Derivative(double sigmoidOutput)
+        {
+            return sigmoidOutput * (1.0 - sigmoidOutput);
+        }
     }
 }
