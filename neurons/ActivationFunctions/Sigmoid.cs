5e2c0154b04ab75b339337a581e35595a16893be
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-02 03:59:14 +0200

    feat: implementa classe Neuron con funzione di attivazione Sigmoid

---
+++ neurons/ActivationFunctions/Sigmoid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace neurons.ActivationFunctions
+{
+    public class Sigmoid
+    {
+        public static double Activate(double sum)
+        {
+            return 1.0 / (1.0 + Math.Exp(-sum));
+        }
+    }
+}
---
+++ neurons/Neuron.cs
@@ -0,0 +1,34 @@
+﻿using neurons.ActivationFunctions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace neurons
+{
+    public class Neuron
+    {
+        public double[] Weights;
+        public double Bias;
+
+        public double Activate(int f, double[] inputs) { 
+        
+            double sum = Bias;
+
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                sum += Weights[i] * inputs[i];
+            }
+
+            switch (f)
+            {
+                // 0 = Funzione di attivazione Sigmoide
+                case 0:
+                    return Sigmoid.Activate(sum);
+                default:
+                    throw new ArgumentException("Funzione di attivazione non valida");
+            }
+
+        }
+
+    }
+}
